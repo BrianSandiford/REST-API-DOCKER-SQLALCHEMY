52140987fdb80754fffb28ae01800c26ad561a38
EC2 Default User <ec2-user@ip-172-31-9-153.us-east-2.compute.internal>
2021-03-20 02:29:23 +0000

    routes updated to Flask-RESTX

--- src/example/app.py
+++ src/example/app.py
@@ -1,15 +1,19 @@
 import json
 
 from flask import request
-
+from flask_restx import Resource, Api
 from . import create_app, database
 from .models import Cats
 
 app = create_app()
+api = Api(app)
 
 
-@app.route('/', methods=['GET'])
-def fetch():
+#@app.route('/', methods=['GET'])
+@api.route('/')
+class GetCats(Resource):
+#def fetch():
+ def get(self):
     cats = database.get_all(Cats)
     all_cats = []
     for cat in cats:
@@ -24,26 +28,30 @@ def fetch():
     return json.dumps(all_cats), 200
 
 
-@app.route('/add', methods=['POST'])
-def add():
+#@app.route('/add', methods=['POST'])
+@api.route('/add')
+class PutCats(Resource):
+ def post(self):
     data = request.get_json()
     name = data['name']
     price = data['price']
     breed = data['breed']
 
     database.add_instance(Cats, name=name, price=price, breed=breed)
-    return json.dumps("Added"), 200
+    return json.dumps("Added"), 201
 
-
-@app.route('/remove/<cat_id>', methods=['DELETE'])
+'''
+#@app.route('/remove/<cat_id>', methods=['DELETE'])
+@api.route('/remove/<cat_id>', methods=['DELETE'])
 def remove(cat_id):
     database.delete_instance(Cats, id=cat_id)
     return json.dumps("Deleted"), 200
 
-
-@app.route('/edit/<cat_id>', methods=['PATCH'])
+#@app.route('/edit/<cat_id>', methods=['PATCH'])
+@api.route('/edit/<cat_id>', methods=['PATCH'])
 def edit(cat_id):
     data = request.get_json()
     new_price = data['price']
     database.edit_instance(Cats, id=cat_id, price=new_price)
     return json.dumps("Edited"), 200
+'''
--- src/start.py
+++ src/start.py
@@ -1,5 +1,5 @@
-#from flask_script import Manager
-#from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from example.app import app
 from example.models import db
 from flask.cli import FlaskGroup
@@ -10,12 +10,12 @@ except:
  print('still not working')
 
 
-#manager = Manager(app)
+manager = Manager(app)
 migrate = Migrate(app, db)
 
-#manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-#   manager.run()
-    cli()  
+   manager.run()
+#    cli()  
