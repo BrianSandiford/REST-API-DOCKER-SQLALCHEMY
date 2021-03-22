1a3235f63942a611598633e1b9c457b9a51dc52d
EC2 Default User <ec2-user@ip-172-31-9-153.us-east-2.compute.internal>
2021-03-22 00:32:13 +0000

    updated test files

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,7 +6,7 @@ from example.models import db
 
 @pytest.fixture(scope='module')
 def test_app():
-    app.config.from_object('src.config.TestingConfig')
+#    app.config.from_object('src.config.TestingConfig')
     with app.app_context():
         yield app  # testing happens here
 
---
+++ src/tests/test_get.py
@@ -0,0 +1,13 @@
+import json
+
+
+def test_ping(test_app):
+    client = test_app.test_client()
+    resp = client.get('/home')
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 200
+    for item in data:
+        if item[0] == 'price':
+            assert  item["price"] == 5000
+#    assert "catty mcCatFace" ==  data["name"]
+#    assert 5000 in data['price']
