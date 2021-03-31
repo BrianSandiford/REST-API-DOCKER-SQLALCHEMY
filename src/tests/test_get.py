f40bf193011805bd5b24427ecc5569904390d2c7
root <root@jenkins.localdomain>
2021-03-31 23:08:36 +0000

    change price to test fail

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -5,8 +5,8 @@ def test_ping(test_app):
     client = test_app.test_client()
     resp = client.get('/')
     data = json.loads(resp.data.decode())
-    assert resp.status_code == 201
+    assert resp.status_code == 200
     for item in data:
-         assert  item["name"] == "catty mcCatFace"
-         assert  item["price"] == 5000
-         assert  item["breed"] == "bengal"
+        # assert  item["name"] == "catty mcCatFace"
+         assert  item["price"] == 2000
+        # assert  item["breed"] == "bengal"
