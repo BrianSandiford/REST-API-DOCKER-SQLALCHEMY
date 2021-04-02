14425b0471eefd9a4ed2a92b4d38a6149ef75440
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-02 22:14:55 +0000

    invalid json test added to test_post.py

--- src/tests/test_post.py
+++ src/tests/test_post.py
@@ -14,3 +14,13 @@ def test_add_cat(test_app, test_database):
     data = json.loads(resp.data.decode())
     assert resp.status_code == 201
     assert 'Added' in data
+
+def test_add_cat_invalid_json(test_app, test_database):
+    client = test_app.test_client()
+    resp = client.post(
+        '/add',
+        data=json.dumps({}),
+        content_type='application/json',
+    )
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
