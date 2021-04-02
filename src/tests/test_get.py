33abd0ebb4d3cf6297e8432f4a3c2d920e1b2f76
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-02 14:10:59 +0000

    testing test_get.py

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -6,8 +6,8 @@ def test_ping(test_app):
     resp = client.get('/')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
-    data[0] == "catty mcCatFace"
-    #for item in data:
-        # assert  item["name"] == "catty mcCatFace"
-        # assert  item["price"] == 2000
-        # assert  item["breed"] == "bengal"
+   # data[0] == "catty mcCatFace"
+    for item in data:
+         assert  item["name"] == "catty mcCatFace"
+         assert  item["price"] == 2000
+         assert  item["breed"] == "bengal"
