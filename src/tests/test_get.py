78b20665d42c5ce55444c57c3fc8a8b0eda47ea8
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-02 14:45:43 +0000

    testing test_get.py

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -3,6 +3,15 @@ import json
 
 def test_ping(test_app):
     client = test_app.test_client()
+    resp0 = client.post(
+        '/add',
+        data=json.dumps({
+            'name': 'catty mcCatFace',
+            'price': 5000,
+            'breed': 'bengal'
+        }),
+        content_type='application/json',
+    ) 
     resp = client.get('/')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
