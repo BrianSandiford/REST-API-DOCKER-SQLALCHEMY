a0a3bfea9e7739946a7ae1f59c11da2563320cbb
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-04 15:31:02 +0000

    added extra asserts in test_get.py

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -24,6 +24,7 @@ def test_get_cat(test_app):
 
 def test_all_cats(test_app, test_database, add_cat):
     add_cat("catty mcCatFace", 5000, "bengal")
+    add_cat("Fluff", 4000, "Siamese")
     client = test_app.test_client()
     resp = client.get('/')
     data = json.loads(resp.data.decode())
@@ -32,3 +33,7 @@ def test_all_cats(test_app, test_database, add_cat):
     assert  "catty mcCatFace" in  data[0]["name"]
     assert  5000 == data[0]["price"]
     assert "bengal" in  data[0]["breed"]
+    assert  "Fluff" in  data[1]["name"]
+    assert  4000 == data[1]["price"]
+    assert "Siamese" in  data[1]["breed"]
+
