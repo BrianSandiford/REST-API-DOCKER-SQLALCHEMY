771589cd2ec01e8887da7002d87056b0ada9438b
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-04 15:20:15 +0000

    changed len == 2 for fail in test_get.py

--- src/tests/test_get.py
+++ src/tests/test_get.py
@@ -28,7 +28,7 @@ def test_all_cats(test_app, test_database, add_cat):
     resp = client.get('/')
     data = json.loads(resp.data.decode())
     assert resp.status_code == 200
-    assert len(data) == 1
+    assert len(data) == 2
     assert  "catty mcCatFace" in  data[0]["name"]
     assert  5000 == data[0]["price"]
     assert "bengal" in  data[0]["breed"]
