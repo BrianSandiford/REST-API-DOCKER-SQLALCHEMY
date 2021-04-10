fcbff6b12009cc4dd7fee6909d01f30358e0079a
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-10 23:31:12 +0000

    removed lastname field from model

--- src/example/models.py
+++ src/example/models.py
@@ -9,7 +9,6 @@ class Cats(db.Model):
     __tablename__ = 'cats'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
     price = db.Column(db.Integer)
     breed = db.Column(db.String(100))
     
