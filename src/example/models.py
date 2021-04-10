2f464142abd1867f7e118f1eda9ce4300694bec4
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-10 20:29:09 +0000

    database migration test

--- src/example/models.py
+++ src/example/models.py
@@ -2,11 +2,14 @@ import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
 
+class DummyTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 class Cats(db.Model):
     __tablename__ = 'cats'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
     price = db.Column(db.Integer)
     breed = db.Column(db.String(100))
     
