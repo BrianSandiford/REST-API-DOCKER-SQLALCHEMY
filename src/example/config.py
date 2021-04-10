65855f70167de264a82838f57cbd8f27472b1bda
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-10 23:48:26 +0000

    added empty sqlite database

--- src/example/config.py
+++ src/example/config.py
@@ -6,9 +6,10 @@ host = 'postgres'
 database = 'postgresdb'
 port = 5432
 
-DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
+#DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}'
 #DATABASE_CONNECTION_URI = f'postgresql+psycopg2://{admin}:{admin123}@{postgres}:{postgres}/{postgresdb}'
-#DATABASE_CONNECTION_URI = 'sqlite:///'  empty database to jumpstar migration on existing project
+DATABASE_CONNECTION_URI = 'sqlite:///'
+  empty database to jumpstar migration on existing project
 class BaseConfig:
     TESTING = False
 
