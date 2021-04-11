cc3ae31963411b3d8b17c9f5d75d1fe36f883c14
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-11 00:13:02 +0000

    added row in dummy table

--- src/example/models.py
+++ src/example/models.py
@@ -4,7 +4,7 @@ db = flask_sqlalchemy.SQLAlchemy()
 
 class DummyTable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
+    name = db.Column(db.String(100))
 class Cats(db.Model):
     __tablename__ = 'cats'
     id = db.Column(db.Integer, primary_key=True)
