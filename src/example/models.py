4658fb85396d9da946c62f2aca7bee2b47b874c1
EC2 Default User <ec2-user@jenkins.localdomain>
2021-04-11 00:30:45 +0000

    removed empty sqlite data and dummy table

--- src/example/models.py
+++ src/example/models.py
@@ -2,9 +2,6 @@ import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
 
-class DummyTable(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
 class Cats(db.Model):
     __tablename__ = 'cats'
     id = db.Column(db.Integer, primary_key=True)
